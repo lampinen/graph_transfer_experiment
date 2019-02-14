a519436067f35b923d23b143bcd848b0cf9bd417
Andrew Lampinen <lampinen@stanford.edu>
2019-02-14 11:19:23 -0800

    Changing to symmetric representation

--- web/js/graphs_and_walks.js
+++ web/js/graphs_and_walks.js
@@ -17,8 +17,8 @@ function range(start, end) {
 }
 
 // Graphs /////////////////////////////////////////////////////////////////////
-// Undirected graphs are represented by an adjacency list of lists, where each
-// nodes list contains the higher numbered nodes it is adjacent to.
+// Graphs are represented by an adjacency array of arrays, where each node's 
+// array contains the nodes it is adjacent to.
 
 
 // generates a n-clique (fully-connected subgraph containing n nodes) starting
@@ -27,7 +27,7 @@ function clique(n, start_index) {
     var edges = [];
     var end_index = start_index + n; // end + 1, actually
     for (var i=start_index; i < end_index; i++) {
-        edges.push(range(i+1, end_index));
+        edges.push(range(start_index, i).concat(range(i+1, end_index)));
     }
     return edges;
 }
@@ -44,10 +44,18 @@ function three_rooms() {
     for (var i=0; i < 3; i++) {
         this_clique = clique(5, 5*i);
         this_clique[0].pop(); // no edge between entry nodes
+        this_clique[4].shift(); // no edge between entry nodes
         this.edges = this.edges.concat(this_clique);
     }
     // room adjacencies
     this.edges[0].push(14);
+    this.edges[14].push(0);
     this.edges[4].push(5);
+    this.edges[5].push(4);
     this.edges[9].push(10);
+    this.edges[10].push(9);
 }
+
+
+//Lattice (from Kahn et al. 2018)
+
