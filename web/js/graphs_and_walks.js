28ef172927cced0bd4ae5f9b5357778f76bf04a1
Andrew Lampinen <lampinen@stanford.edu>
2019-02-14 11:05:00 -0800

    Three rooms

--- web/js/graphs_and_walks.js
+++ web/js/graphs_and_walks.js
@@ -25,7 +25,7 @@ function range(start, end) {
 // from start_index.
 function clique(n, start_index) {
     var edges = [];
-    end_index = start_index + n; // end + 1, actually
+    var end_index = start_index + n; // end + 1, actually
     for (var i=start_index; i < end_index; i++) {
         edges.push(range(i+1, end_index));
     }
@@ -39,9 +39,15 @@ function three_rooms() {
     this.name = "three_rooms";
     this.nodes = range(num_nodes);
     this.edges = [];
+    var this_clique;
+    // rooms/almost cliques
+    for (var i=0; i < 3; i++) {
+        this_clique = clique(5, 5*i);
+        this_clique[0].pop(); // no edge between entry nodes
+        this.edges = this.edges.concat(this_clique);
+    }
+    // room adjacencies
+    this.edges[0].push(14);
+    this.edges[4].push(5);
+    this.edges[9].push(10);
 }
-
-three_rooms.prototype.get_name = function() {
-    return this.name;
-}
-
