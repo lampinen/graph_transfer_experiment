9ee732cc75fdd430c7257e78c05f7809d77a3ffe
Andrew Lampinen <lampinen@stanford.edu>
2019-03-08 17:29:05 -0800

    Tweaks

--- utils/download.sh
+++ utils/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 host=lampinen@rice
 datadir='~/afs-home/cgi-bin/data/gl'
-subdir=pilot
+subdir=ex0
 raw_target=../raw_data
 anon_target=../anonymized_data
 
@@ -14,10 +14,15 @@ scp ${host}:${datadir}/${subdir}/*.json ${raw_target}/${subdir}/
 # anonymize
 mkdir -p ${anon_target}/${subdir}
 
+dictionary=${raw_target}/${subdir}/dictionary.csv
+echo 'filename, index\n' > $dictionary
+
 i=1
 for f in ${raw_target}/${subdir}/*.json
 do
+    filename=$(basename ${f})
     cp ${f} ${anon_target}/${subdir}/${i}.json
+    echo $filename, ${i} >> $dictionary
     i=$((i+1))
 done
 
